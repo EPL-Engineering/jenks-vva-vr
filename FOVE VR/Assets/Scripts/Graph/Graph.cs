bd9c24f1abb8122707665592767cdaf18d64a898
keh38 <Ken_Hancock@meei.harvard.edu>
2024-08-30 14:08:23 -0400

    bang!

---
+++ FOVE VR/Assets/Scripts/Graph/Graph.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    public UdpSignal UdpSignal;
+    private float[] data;
+
+    List<GameObject> lineList = new List<GameObject>();
+
+    private DD_DataDiagram m_DataDiagram;
+    //private RectTransform DDrect;
+
+    private float m_Input = 0f;
+
+    void AddALine(Color color)
+    {
+        if (null == m_DataDiagram)
+            return;
+
+        //float h = Random.value;
+        //Color color = Color.HSVToRGB((h += 0.1f) > 1 ? (h - 1) : h, 0.8f, 0.8f);
+        GameObject line = m_DataDiagram.AddLine(color.ToString(), color);
+        lineList.Add(line);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject dd = GameObject.Find("DataDiagram");
+        if (null == dd)
+        {
+            Debug.LogWarning("can not find a gameobject of DataDiagram");
+            return;
+        }
+        m_DataDiagram = dd.GetComponent<DD_DataDiagram>();
+        m_DataDiagram.PreDestroyLineEvent += (s, e) => { lineList.Remove(e.line); };
+
+
+        AddALine(Color.white);
+        AddALine(Color.green);
+        //AddALine();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        m_Input += Time.deltaTime;
+        data = UdpSignal.signalData;
+        ContinueInput(m_Input);
+    }
+
+    private void ContinueInput(float f)
+    {
+        if (null == m_DataDiagram)
+            return;
+        m_DataDiagram.InputPoint(lineList[0], new Vector2(0.01f, 511));
+        m_DataDiagram.InputPoint(lineList[1], new Vector2(0.01f, (data[0]+511/2)));
+
+
+    }
+}
---
+++ FOVE VR/Assets/Scripts/HeadTilt/TransController.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.VR;
+using UnityEngine.XR;
+using InputTracking = UnityEngine.XR.InputTracking;
+
+public class TransController : MonoBehaviour
+{
+
+    public Camera childCamera;
+    public Vector3 RotateAmount;
+    public UdpSignal UDPdata; //from WiFi Arduino
+    public float rawTiltValue;
+    public float tiltValue;
+    public float dc_offset = 2565f;
+    public float Gain_voltage_to_angle = 0.0366f;
+    public float Gain_angle_to_translation = 0.3f;
+    private float translationValue;
+    public float UDP_counter;
+
+
+    //Low Pass Filter
+    protected Queue<float> filterDataQueue = new Queue<float>();
+    public int filterLength = 50;
+    protected float filteredTranslationValue = 0;
+
+    UnityEngine.XR.InputDevice ViveHMD;
+
+    //Current
+    UnityEngine.Vector3 ViveHMDPosition;
+    UnityEngine.Quaternion ViveHMDRotation;
+    UnityEngine.Vector3 ViveHMDVelocity;
+    UnityEngine.Vector3 ViveHMDAngularVelocity;
+
+    //Previous
+    UnityEngine.Vector3 preViveHMDPosition;
+    UnityEngine.Quaternion preViveHMDRotation;
+    UnityEngine.Vector3 preViveHMDVelocity;
+    UnityEngine.Vector3 preViveHMDAngularVelocity;
+
+    private void Awake()
+    {
+        XRDevice.DisableAutoXRCameraTracking(childCamera, true);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        var inputDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevices(inputDevices);
+
+        foreach (var device in inputDevices)
+        {
+            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.characteristics.ToString()));
+            if (device.name == "Vive DVT")
+            {
+                ViveHMD = device;
+                Debug.Log(string.Format("Found the Vive! it'd called '{0}'", ViveHMD.name));
+
+                var inputFeatures = new List<UnityEngine.XR.InputFeatureUsage>();
+                if (device.TryGetFeatureUsages(inputFeatures))
+                {
+                    foreach (var feature in inputFeatures)
+                    {
+                        Debug.Log(string.Format(" feature {0}'s type is {1}, other data {2}", feature.name, feature.type, feature.ToString()));
+                    }
+                }
+
+                UpdateCoordinates();
+                PrintCoordinates();
+            }
+            break;
+        }
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCoordinates();
+
+        //if changed print
+        if (preViveHMDPosition != ViveHMDPosition ||
+            preViveHMDRotation != ViveHMDRotation ||
+            preViveHMDVelocity != ViveHMDVelocity ||
+            preViveHMDAngularVelocity != ViveHMDAngularVelocity)
+        { PrintCoordinates(); }
+
+        if (UDPdata.signalData[0] != 0)
+        {   
+            rawTiltValue = UDPdata.signalData[1];
+            print(UDPdata.signalData[0]);
+            UDP_counter = UDPdata.signalData[0];
+            UDPdata.signalData[0] = 0;
+        }
+        
+
+        // Arduino Uno 1 bit is ..4.9mV. Home is about 2.5V.
+        tiltValue = (rawTiltValue * 4.9f - dc_offset) * Gain_voltage_to_angle;
+        translationValue = tiltValue * Gain_angle_to_translation;
+
+        //Low pass filter
+        filterDataQueue.Enqueue(translationValue);        
+        if(filterDataQueue.Count > filterLength )
+        { filterDataQueue.Dequeue(); }
+        foreach (float f in filterDataQueue)
+            filteredTranslationValue += f;
+        filteredTranslationValue /= filterLength;
+
+        transform.position = new Vector3(filteredTranslationValue, 0, 0);
+
+        //Inverse Kinematics - negate HMD tracking - may be useful later.
+        //transform.rotation = Quaternion.Inverse(ViveHMDRotation);
+        //transform.position = -(transform.rotation * ViveHMDPosition);
+
+    }
+
+
+
+
+
+    void PrintCoordinates()
+    {
+        //Debug.Log("Position " + ViveHMDPosition.ToString("F8") + 
+        //         " Rotaton " + ViveHMDRotation.ToString("F8") +  
+        //         " Velocity " + ViveHMDVelocity.ToString("F8") +
+        //         " Angular Velocity" + ViveHMDAngularVelocity.ToString("F8"));
+    }
+
+    void UpdateCoordinates()
+    {
+        //Previous
+        preViveHMDPosition = ViveHMDPosition;
+        preViveHMDRotation = ViveHMDRotation;
+        preViveHMDVelocity = ViveHMDVelocity;
+        preViveHMDAngularVelocity = ViveHMDAngularVelocity;
+
+
+        //ViveHMD.TryGetFeatureValue(UnityEngine.XR.CommonUsages.devicePosition, out ViveHMDPosition);
+        //ViveHMD.TryGetFeatureValue(UnityEngine.XR.CommonUsages.deviceRotation, out ViveHMDRotation);
+        //ViveHMD.TryGetFeatureValue(UnityEngine.XR.CommonUsages.deviceVelocity, out ViveHMDVelocity);
+        //ViveHMD.TryGetFeatureValue(UnityEngine.XR.CommonUsages.deviceAngularVelocity, out ViveHMDAngularVelocity);
+        ViveHMD.TryGetFeatureValue(UnityEngine.XR.CommonUsages.centerEyePosition, out ViveHMDPosition);
+        ViveHMD.TryGetFeatureValue(UnityEngine.XR.CommonUsages.centerEyeRotation, out ViveHMDRotation);
+        ViveHMD.TryGetFeatureValue(UnityEngine.XR.CommonUsages.centerEyeVelocity, out ViveHMDVelocity);
+        ViveHMD.TryGetFeatureValue(UnityEngine.XR.CommonUsages.centerEyeAngularVelocity, out ViveHMDAngularVelocity);
+
+    }
+}
+
